5370d78f23deb04272455c5a41c485d215b07100
nguyenbinh <trannguyenbinh.shark@gmail.com>
2026-08-10 11:12:51 +0700

    Mark subprojects as dirty in interbotix_ros_core, interbotix_ros_manipulators, and interbotix_ros_toolboxes

--- gen_fit_and_3d_graph/fuzzy_type1_demo.c
+++ gen_fit_and_3d_graph/fuzzy_type1_demo.c
@@ -1,5 +1,5 @@
-/* Auto-generated demo for FIS "fuzzy_type11". Reads 2 input(s) from argv, prints 1 output(s). */
-#include "fuzzy_type11.h"
+/* Auto-generated demo for FIS "fuzzy_type1". Reads 2 input(s) from argv, prints 1 output(s). */
+#include "fuzzy_type1.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +11,7 @@ int main(int argc, char ** argv) {
     float in[2];
     for (int i = 0; i < 2; i++) in[i] = (float) atof(argv[i + 1]);
     float out[1];
-    fuzzy_type11_eval_core(in, out);
+    fuzzy_type1_eval_core(in, out);
     for (int i = 0; i < 1; i++)
         printf("%g%c", out[i], (i + 1 < 1) ? ' ' : '\n');
     return 0;
